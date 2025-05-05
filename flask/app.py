c4ce648bbdf9bab00f89167747904755c5f2d8f9
yunpeng7 <yunpeng7@staff.weibo.com>
2025-05-05 09:13:56 +0800

    Add loading state and integrate API for fetching debates in HotDebates view

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 # from debateck import debateck
 from debates import debateck
 from ttv import ttv
@@ -6,6 +7,8 @@ from debatefromnews import debatefromnews
 from photo import photo
 
 app = Flask(__name__)
+# 添加CORS支持，允许所有来源的跨域请求
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(debateck)
 app.register_blueprint(ttv)
 app.register_blueprint(debatefromnews)
